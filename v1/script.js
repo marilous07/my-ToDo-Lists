dc1e8301d297905102b90f509fc7126fdd630b26
Marilena Mousteri <73708683+marilous07@users.noreply.github.com>
2023-11-01 10:16:54 +0200

    Update script.js
    add date and priority fucntion

--- v1/script.js
+++ v1/script.js
@@ -1,18 +1,28 @@
 // Get references to HTML elements
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
+const dueDate = document.getElementById('dueDate');
+const priority = document.getElementById('priority');
 
 // Function to add a new task
 function addTask() {
     const taskText = taskInput.value.trim();
+    const taskDate = dueDate.value;
+    const taskPriority = priority.value;
     if (taskText !== '') {
         const li = document.createElement('li');
         li.innerHTML = `
-            ${taskText}
+          <div class="task-info">
+                <p>${taskText}</p>
+                <p>Due Date: ${taskDate}</p>
+                <p>Priority: ${taskPriority}</p>
+            </div>
             <button class="delete-btn" onclick="removeTask(this)">Delete</button>
         `;
         taskList.appendChild(li);
         taskInput.value = '';
+        dueDate.value = '';
+        priority.value = 'low';
     }
 }
 
